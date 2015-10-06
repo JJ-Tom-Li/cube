2de13f4444deecb2e8b7236b83ef30a57c219a3b
pandatomlcc <pandatomlcc@users.noreply.github.com>
2015-10-06 21:32:14 +0800

    Update a016(unfinish).cpp

--- a016(unfinish).cpp
+++ a016(unfinish).cpp
@@ -12,33 +12,63 @@ while (scanf("%d",&n[0][0])!=EOF){
 		}
 	} 
 	
-	for (i=0;i<9;i++){      //確認橫列sum=9
+	for (i=0;i<9;i++){        //確認橫列sum=45 
 		for (j=0;j<9;j++){
 			sum=sum+n[i][j];
 		}
-		if (sum!=9){
+		if (sum!=45){
 			sum=0;
 			break;
 		}
-		else if (i==8&&sum==9){
+		else if (i==8&&sum==45){
 			check++;
 		}
 		sum=0;
 	}
 	
-	for (i=0;i<9;i++){      ////確認直列sum=9
+	for (i=0;i<9;i++){      //確認直列sum=45
 		for (j=0;j<9;j++){
 			sum=sum+n[j][i];
 		}
-		if (sum!=9){
+		if (sum!=45){
 			sum=0;
 			break;
 		}
-		else if (i==8&&sum==9){
+		else if (i==8&&sum==45){
 			check++;
 		}
 		sum=0;
 	}
+	
+	int count=1,start1=0,start2=0;
+	for (i=start1;i<=start1+2&&i<9;i++){    //確認9宮格sum=45
+		for (j=start2;j<=start2+2&&j<9;j++){
+			sum=sum+n[i][j]; 
+		}
+		if (i==start1+2&&sum!=45){
+			sum=0;
+			break; 
+		}
+		if (i==start1+2){
+			start1+=3;
+			count++; 
+		}
+		if (count==3){
+			start2+=3;
+			count=1; 
+		}
+		if (i==8&&count==3&&sum==45){
+			check++; 
+		}
+		sum=0; 
+	} 
+	
+	
+	if (check==3){
+		printf("yes\n");
+	}
+	else
+		printf("no\n"); 
 	
 	
 	
@@ -53,3 +83,5 @@ while (scanf("%d",&n[0][0])!=EOF){
 
 	return 0;
 }
+
+
